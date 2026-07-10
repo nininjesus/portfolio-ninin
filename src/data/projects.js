4ddbcca5e5eb2731939aacfeb048a8c995d8db9b
Jesús Ninin <nj13072004@gmail.com>
2026-07-10 15:54:36 -0400

    Fase 5, Desarrollo de componentes
    Se cumplio la fase, referente al desarrollo de componentes. Queda aun la fase relacionada al responsive

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,22 @@
 export const projectsList = [
-  // { id: 'proyecto-01', title: 'proyecto_01.tgz.gz', description: 'Descripción', year: 2024, category: 'Diseño Web', urls: {} }
+  { 
+    id: 'proyecto-01', 
+    title: 'proyecto_01.tgz.gz', 
+    description: 'Diseño Web | 2024', 
+    year: 2024, 
+    category: 'Diseño Web', 
+    thumbnailSrc: undefined, 
+    urlView: '#', 
+    urlPreview: '#' 
+  },
+  { 
+    id: 'proyecto-02', 
+    title: 'proyecto_02.tgz.gz', 
+    description: 'Desarrollo Web | 2023', 
+    year: 2023, 
+    category: 'Desarrollo Web', 
+    thumbnailSrc: undefined, 
+    urlView: '#', 
+    urlPreview: '#' 
+  }
 ];
