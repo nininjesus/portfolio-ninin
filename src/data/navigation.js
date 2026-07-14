0dcadc9f7b19adfb0a12f2ddf869562503d8dfe9
Jesús Ninin <nj13072004@gmail.com>
2026-07-14 11:11:32 -0400

    fix: Estructura base
    Se mejoro la estructura base del proyecto, para obtener un resultado mas fiel a la maquetacion

--- src/data/navigation.js
+++
@@ -1,7 +0,0 @@
-export const navItems = [
-  { index: 1, label: 'ninin', sectionId: 'ninin', description: 'Muestra información sobre el autor' },
-  { index: 2, label: 'la proyectos', sectionId: 'la-proyectos', description: 'Lista los proyectos desarrollados' },
-  { index: 3, label: 'sobre_mi', sectionId: 'sobre-mi', description: 'Muestra el stack de tecnologías' },
-  { index: 4, label: 'mail contacto', sectionId: 'mail-contacto', description: 'Inicia el protocolo de mensajería' },
-  { index: 5, label: 'man ayuda', sectionId: 'man-ayuda', description: 'Abre este manual de usuario' }
-];
