b5ded99cd794fa1caf9460477fe025ee14809bc7
Jesús Ninin <nj13072004@gmail.com>
2026-07-23 14:14:04 -0400

    fix/feature: Optimizacion
    Se optimizaron ciertos procesos y a su vez la navegacion al momento de cambiar entre secciones dependiendo del tamaño de la ventana

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://ninin.online',
   integrations: [sitemap()],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
 });
